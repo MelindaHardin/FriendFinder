dc7217d66b3a4660a3dd1db86ef0edbac9bb4512
Melinda Hardin <melindawad@gmail.com>
2018-04-15 09:33:12 -0500

    first

---
+++ app/data/friends.js
@@ -0,0 +1,1 @@
+/*save your application's data inside as an array of objects. Each of these objects should roughly follow the format below.*/
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+/*include two routes:
+
+A GET Route to /survey 
+which should display the survey page.
+
+
+And a default, catch-all route that leads to home.html which displays the home page.*/
